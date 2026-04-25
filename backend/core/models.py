a5adacc1596b535e7bb310ac910e2f60380f8e47
RohanShelke <shelkerohan555@gmail.com>
2026-04-26 00:16:57 +0530

    Allow spaces in username

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
+
+class SpaceUsernameValidator(RegexValidator):
+    regex = r'^[\w.@+-\s]+$'
+    message = "Enter a valid username. This value may contain only letters, numbers, spaces, and @/./+/-/_ characters."
 
 class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[SpaceUsernameValidator()],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
     class Role(models.TextChoices):
         MERCHANT = 'merchant', 'Merchant'
         REVIEWER = 'reviewer', 'Reviewer'
